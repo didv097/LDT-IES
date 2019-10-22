01424cd8494f0af80f7ecc2453ae9060cd5cc70f
bran0 <bran107@outlook.com>
2019-10-22 13:15:58 -0500

    style

--- ldt2ies.py
+++ ldt2ies.py
@@ -6,11 +6,11 @@ def num2str(n):
 	return "{:g}".format(n)
 
 company_name = f_ldt.readline()
-ltyp = int(f_ldt.readline())	# Type indicator
-lsym = int(f_ldt.readline())	# Symmetry indicator
-mc = int(f_ldt.readline())		# Number of C-planes between 0 and 360 degrees (usually 24 for interior, 36 for road lighting luminaires)
-dc = float(f_ldt.readline())	# Distance between C-planes
-ng = int(f_ldt.readline())		# Number of luminous intensities in each C-plane
+ltyp = int(f_ldt.readline())		# Type indicator
+lsym = int(f_ldt.readline())		# Symmetry indicator
+mc = int(f_ldt.readline())			# Number of C-planes between 0 and 360 degrees (usually 24 for interior, 36 for road lighting luminaires)
+dc = float(f_ldt.readline())		# Distance between C-planes
+ng = int(f_ldt.readline())			# Number of luminous intensities in each C-plane
 dg = float(f_ldt.readline())		# Distance between luminous intensities per C-plane
 meas_report = f_ldt.readline()	# Measurement report number
 lum_name = f_ldt.readline()			# Luminaire name
@@ -23,34 +23,34 @@ if temp >= 0:
 	date = date[0 : temp - 1] + "\n"
 else:
 	user = "\n"
-length_lum = float(f_ldt.readline())		# Length/diameter of luminaire (mm)
-width_lum = float(f_ldt.readline())		# b - Width of luminaire (mm) (b = 0 for circular luminaire)
-height_lum = float(f_ldt.readline())		# Height of luminaire (mm)
-length_lumarea = float(f_ldt.readline())				# Length of luminaire (mm)
+length_lum = float(f_ldt.readline())					# Length/diameter of luminaire (mm)
+width_lum = float(f_ldt.readline())						# b - Width of luminaire (mm) (b = 0 for circular luminaire)
+height_lum = float(f_ldt.readline())					# Height of luminaire (mm)
+length_lumarea = float(f_ldt.readline())			# Length of luminaire (mm)
 width_lumarea = float(f_ldt.readline())				# Width of luminous area (mm) (b1 = 0 for circular luminous area of luminaire)
 height_lumarea_c0 = float(f_ldt.readline())		# Height of luminous area C0-plane (mm)
-height_lumarea_c90 = float(f_ldt.readline())		# Height of luminous area C90-plane (mm)
+height_lumarea_c90 = float(f_ldt.readline())	# Height of luminous area C90-plane (mm)
 height_lumarea_c180 = float(f_ldt.readline())	# Height of luminous area C180-plane (mm)
 height_lumarea_c270 = float(f_ldt.readline())	# Height of luminous area C270-plane (mm)
-dff = float(f_ldt.readline())		# DFF - Downward flux fraction (%)
-lorl = float(f_ldt.readline())		# LORL - Light output ratio luminaire (%)
-cffli = float(f_ldt.readline())	# Conversion factor for luminous intensities (depending on measurement)
-toldm = float(f_ldt.readline())	# Tilt of luminaire during measurement (road lighting luminaires)
-num_set = int(f_ldt.readline())		# n - Number of standard sets of lamps
-num_lam = int(f_ldt.readline())		# Number of lamps
-type_lam = f_ldt.readline()				# Type of lamps
-tlfl = float(f_ldt.readline())				# Total luminous flux of lamps (lm)
-col_temp = float(f_ldt.readline())		# Color appearance / color temperature of lamps
+dff = float(f_ldt.readline())				# DFF - Downward flux fraction (%)
+lorl = float(f_ldt.readline())			# LORL - Light output ratio luminaire (%)
+cffli = float(f_ldt.readline())			# Conversion factor for luminous intensities (depending on measurement)
+toldm = float(f_ldt.readline())			# Tilt of luminaire during measurement (road lighting luminaires)
+num_set = int(f_ldt.readline())			# n - Number of standard sets of lamps
+num_lam = int(f_ldt.readline())			# Number of lamps
+type_lam = f_ldt.readline()					# Type of lamps
+tlfl = float(f_ldt.readline())			# Total luminous flux of lamps (lm)
+col_temp = float(f_ldt.readline())	# Color appearance / color temperature of lamps
 col_ren = float(f_ldt.readline())		# Color rendering group / color rendering index
 power = float(f_ldt.readline())			# Wattage including ballast (W)
 dr = []
-for i in range(0, 10):		# DR - Direct ratios for room indices k = 0.6 ... 5
+for i in range(0, 10):							# DR - Direct ratios for room indices k = 0.6 ... 5
 	dr.append(float(f_ldt.readline()))
 angle_c = []
-for i in range(0, mc):		# Angles C (beginning with 0 degrees)
+for i in range(0, mc):							# Angles C (beginning with 0 degrees)
 	angle_c.append(float(f_ldt.readline()))
 angle_g = []
-for i in range(0, ng):		# Angles G (beginning with 0 degrees)
+for i in range(0, ng):							# Angles G (beginning with 0 degrees)
 	angle_g.append(float(f_ldt.readline()))
 lum_intensity = []
 for i in range(0, mc):
