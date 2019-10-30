d82c80b9c58de3edddc59d2a35755aee01ffe275
bran0 <bran107@outlook.com>
2019-10-29 22:31:56 -0500

    add comments

--- ies_interpolate.py
+++ ies_interpolate.py
@@ -1,21 +1,20 @@
 import sys
 
-new_cnt_ver = int(sys.argv[1])
-new_cnt_hor = int(sys.argv[2])
+new_cnt_ver = int(sys.argv[1])			# first parameter - new number of vertical angles
+new_cnt_hor = int(sys.argv[2])			# second parameter - new number of horizontal angles
 
 f_org = open("original.ies", "r")
 f_res = open("result.ies", "w")
 
-def num2str(n):
+def num2str(n):											# convert number to string
 	s = str(int(n * 10000) / 10000)
 	if '.' in s:
 		return s.rstrip('0').rstrip('.')
 	else:
 		return s
 
-f_res.write(f_org.readline())
-
-while True:
+f_res.write(f_org.readline())				# write lines that are unnecessary to change
+while True:													#
 	line = f_org.readline()
 	f_res.write(line)
 	if line[0] != '[':
@@ -23,43 +22,32 @@ while True:
 
 line = f_org.readline()
 items = line.split()
-org_cnt_ver = int(items[3])
-org_cnt_hor = int(items[4])
-if new_cnt_ver < 1:
+org_cnt_ver = int(items[3])					# read number of angles
+org_cnt_hor = int(items[4])					# 
+if new_cnt_ver < 1:									# validate parameters
 	new_cnt_ver = org_cnt_ver
-if new_cnt_hor < 1:
+if new_cnt_hor < 1:									# validate parameters
 	new_cnt_hor = org_cnt_hor
-items[3] = num2str(new_cnt_ver)
-items[4] = num2str(new_cnt_hor)
+items[3] = num2str(new_cnt_ver)			# update number of angles with new values
+items[4] = num2str(new_cnt_hor)			# 
 f_res.write(" ".join(items) + "\n")
 
 f_res.write(f_org.readline())
 
-line = f_org.readline()
-items = line.split()
-bal_fac = float(items[0])
-fut_use = float(items[1])
-input_watts = float(items[2])
-
-ver_angles = []
-hor_angles = []
-line = f_org.readline()
+line = f_org.readline()	
 max_ver_angle = [float(i) for i in line.split()][-1]
 while True:
 	line = f_org.readline()
 	if line[0] != ' ':
 		break
-	max_ver_angle = [float(i) for i in line.split()][-1]
+	max_ver_angle = [float(i) for i in line.split()][-1]	# maximum vertical angle
 
 max_hor_angle = [float(i) for i in line.split()][-1]
 while True:
 	line = f_org.readline()
 	if line[0] != ' ':
 		break
-	max_hor_angle = [float(i) for i in line.split()][-1]
-
-ver_angles = [i * max_ver_angle / (new_cnt_ver - 1) for i in range(new_cnt_ver)]
-hor_angles = [i * max_hor_angle / (new_cnt_hor - 1) for i in range(new_cnt_hor)]
+	max_hor_angle = [float(i) for i in line.split()][-1]	# maximum horizontal angle
 
 angles = []
 for i in range(org_cnt_hor):
@@ -68,10 +56,10 @@ for i in range(org_cnt_hor):
 		line = f_org.readline()
 		if line == "" or line[0] != ' ':
 			break
-		angles[i] += [float(j) for j in line.split()]
+		angles[i] += [float(j) for j in line.split()]				# read angles
 
 line = ""
-for i in range(new_cnt_ver):
+for i in range(new_cnt_ver):														# write new vertical angles
 	it = num2str(i * max_ver_angle / (new_cnt_ver - 1))
 	if len(line + it) > 238:
 		f_res.write(line + "\n")
@@ -80,7 +68,7 @@ for i in range(new_cnt_ver):
 f_res.write(line + "\n")
 
 line = ""
-for i in range(new_cnt_hor):
+for i in range(new_cnt_hor):														# write new horizontal angles
 	it = num2str(i * max_hor_angle / (new_cnt_hor - 1))
 	if len(line + it) > 238:
 		f_res.write(line + "\n")
@@ -88,13 +76,15 @@ for i in range(new_cnt_hor):
 	line += it + " "
 f_res.write(line + "\n")
 
+# interpolating function
+# https://www.paulinternet.nl/?page=bicubic
 def interpolate(p0, p1, p2, p3, x):
 	return (-0.5 * p0 + 1.5 * p1 - 1.5 * p2 + 0.5 * p3) * x * x * x + \
 		(p0 - 2.5 * p1 + 2 * p2 - 0.5 * p3) * x * x + \
 		(-0.5 * p0 + 0.5 * p2) * x + p1
 
 tmp_angles = []
-for i in range(org_cnt_hor):
+for i in range(org_cnt_hor):			# interpolate vertical angles
 	tmp_angles.append([])
 	for j in range(new_cnt_ver - 1):
 		x = max_ver_angle / (new_cnt_ver - 1) * j
@@ -119,7 +109,7 @@ for i in range(new_cnt_hor):
 		angles[i].append(0)
 
 line = ""
-for j in range(new_cnt_ver):
+for j in range(new_cnt_ver):			# interpolate horizontal angles
 	line = ""
 	for i in range(new_cnt_hor - 1):
 		x = max_hor_angle / (new_cnt_hor - 1) * i
@@ -137,7 +127,7 @@ for j in range(new_cnt_ver):
 		angles[i][j] = interpolate(p0, p1, p2, p3, x / (max_hor_angle / (org_cnt_hor - 1)) % 1)
 	angles[-1][j] = interpolate(tmp_angles[-3][j], tmp_angles[-2][j], tmp_angles[-1][j], tmp_angles[-1][j], 1)
 
-for i in range(new_cnt_hor):
+for i in range(new_cnt_hor):			# write new angles
 	line = ""
 	for j in range(new_cnt_ver):
 		if len(line + num2str(angles[i][j])) > 238:
--- ldt_interpolate.py
+++ ldt_interpolate.py
@@ -1,12 +1,12 @@
 import sys
 
-new_mc = int(sys.argv[1])
-new_ng = int(sys.argv[2])
+new_mc = int(sys.argv[1])						# first parameter - new org_mc
+new_ng = int(sys.argv[2])						# first parameter - new org_ng
 
 f_org = open("original.ldt", "r")
 f_res = open("result.ldt", "w")
 
-def num2str(n):
+def num2str(n):											# convert number to string
 	s = str(int(n * 10000) / 10000)
 	if '.' in s:
 		return s.rstrip('0').rstrip('.')
@@ -16,77 +16,79 @@ def num2str(n):
 for i in range(3):
 	f_res.write(f_org.readline())
 
-mc = int(f_org.readline())			# Number of C-planes between 0 and 360 degrees (usually 24 for interior, 36 for road lighting luminaires)
-dc = float(f_org.readline())		# Distance between C-planes
-ng = int(f_org.readline())			# Number of luminous intensities in each C-plane
-dg = float(f_org.readline())		# Distance between luminous intensities per C-plane
+org_mc = int(f_org.readline())			# Number of C-planes between 0 and 360 degrees (usually 24 for interior, 36 for road lighting luminaires)
+org_dc = float(f_org.readline())		# Distance between C-planes
+org_ng = int(f_org.readline())			# Number of luminous intensities in each C-plane
+org_dg = float(f_org.readline())		# Distance between luminous intensities per C-plane
 
-max_angle_c = mc * dc						# 360
-max_angle_g = (ng - 1) * dg			# 90
+max_angle_c = org_mc * org_dc				# 360
+max_angle_g = (org_ng - 1) * org_dg	# 90
 
-if new_mc < 1:
-	f_res.write(num2str(mc) + "\n")
-	f_res.write(num2str(dc) + "\n")
-	new_mc = mc
+if new_mc < 1:											# validate parameters
+	f_res.write(num2str(org_mc) + "\n")
+	f_res.write(num2str(org_dc) + "\n")
+	new_mc = org_mc
 else:
 	f_res.write(num2str(new_mc) + "\n")
 	f_res.write(num2str(max_angle_c / new_mc) + "\n")
 if new_ng < 1:
-	f_res.write(num2str(ng) + "\n")
-	f_res.write(num2str(dg) + "\n")
-	new_ng = ng
+	f_res.write(num2str(org_ng) + "\n")
+	f_res.write(num2str(org_dg) + "\n")
+	new_ng = org_ng
 else:
 	f_res.write(num2str(new_ng) + "\n")
 	f_res.write(num2str(max_angle_g / (new_ng - 1)) + "\n")
 
-for i in range(35):
+for i in range(35):									# write lines that are unnecessary to change
 	f_res.write(f_org.readline())
 
-for i in range(mc + ng):
+for i in range(org_mc + org_ng):		# read angles C & G
 	f_org.readline()
 
-for i in range(new_mc):
+for i in range(new_mc):							# write new angles C & G
 	f_res.write(num2str(max_angle_c / new_mc * i) + "\n")
 for i in range(new_ng):
 	f_res.write(num2str(max_angle_g / (new_ng - 1) * i) + "\n")
 
 lum_intensity = []
 tmp_lum_intensity = []
-for i in range(mc):
+for i in range(org_mc):							# read luminous intensities
 	lum_intensity.append([])
-	for j in range(ng):
+	for j in range(org_ng):
 		line = f_org.readline()
 		if line == "":
 			break
 		lum_intensity[i].append(float(line))
-	if j < ng - 1:
+	if j < org_ng - 1:
 		lum_intensity.pop()
 		break
 
+# interpolating function
+# https://www.paulinternet.nl/?page=bicubic
 def interpolate(p0, p1, p2, p3, x):
 	return (-0.5 * p0 + 1.5 * p1 - 1.5 * p2 + 0.5 * p3) * x * x * x + \
 		(p0 - 2.5 * p1 + 2 * p2 - 0.5 * p3) * x * x + \
 		(-0.5 * p0 + 0.5 * p2) * x + p1
 
-for i in range(mc):
+for i in range(org_mc):							# interpolate angles G
 	tmp_lum_intensity.append([])
 	for j in range(new_ng - 1):
 		x = max_angle_g / (new_ng - 1) * j
-		it_idx = int(x / dg)
+		it_idx = int(x / org_dg)
 		p1 = lum_intensity[i][it_idx]
 		p2 = lum_intensity[i][it_idx + 1]
 		if it_idx == 0:
 			p0 = p1
 			p3 = lum_intensity[i][it_idx + 2]
-		elif it_idx == ng - 2:
+		elif it_idx == org_ng - 2:
 			p3 = p2
 			p0 = lum_intensity[i][it_idx - 1]
 		else:
 			p0 = lum_intensity[i][it_idx - 1]
 			p3 = lum_intensity[i][it_idx + 2]
-		tmp_lum_intensity[i].append(interpolate(p0, p1, p2, p3, x % dg / dg))
-	tmp_lum_intensity[i].append(interpolate(lum_intensity[i][ng - 3], \
-		lum_intensity[i][ng - 2], lum_intensity[i][ng - 1], lum_intensity[i][ng - 1], 1))
+		tmp_lum_intensity[i].append(interpolate(p0, p1, p2, p3, x % org_dg / org_dg))
+	tmp_lum_intensity[i].append(interpolate(lum_intensity[i][org_ng - 3], \
+		lum_intensity[i][org_ng - 2], lum_intensity[i][org_ng - 1], lum_intensity[i][org_ng - 1], 1))
 
 lum_intensity = []
 for i in range(new_mc):
@@ -94,24 +96,24 @@ for i in range(new_mc):
 	for j in range(new_ng):
 		lum_intensity[i].append(0)
 
-for j in range(new_ng):
+for j in range(new_ng):							# interpolate angles C
 	for i in range(new_mc):
 		x = max_angle_c / new_mc * i
-		it_idx = int(x / dc)
+		it_idx = int(x / org_dc)
 		p1 = tmp_lum_intensity[it_idx][j]
-		p2 = tmp_lum_intensity[(it_idx + 1) % mc][j]
+		p2 = tmp_lum_intensity[(it_idx + 1) % org_mc][j]
 		if it_idx == 0:
 			p0 = p1
-			p3 = tmp_lum_intensity[(it_idx + 2) % mc][j]
-		elif it_idx == mc - 1:
+			p3 = tmp_lum_intensity[(it_idx + 2) % org_mc][j]
+		elif it_idx == org_mc - 1:
 			p3 = p2
 			p0 = tmp_lum_intensity[it_idx - 1][j]
 		else:
 			p0 = tmp_lum_intensity[it_idx - 1][j]
-			p3 = tmp_lum_intensity[(it_idx + 2) % mc][j]
-		lum_intensity[i][j] = interpolate(p0, p1, p2, p3, x % dc / dc)
+			p3 = tmp_lum_intensity[(it_idx + 2) % org_mc][j]
+		lum_intensity[i][j] = interpolate(p0, p1, p2, p3, x % org_dc / org_dc)
 
-for i in range(new_mc):
+for i in range(new_mc):						# write new angles
 	for j in range(new_ng):
 		f_res.write(num2str(lum_intensity[i][j]) + "\n")
 
