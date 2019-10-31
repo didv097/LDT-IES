de6237130514e25127c9bc991071f8bc9b7fb20b
bran0 <bran107@outlook.com>
2019-10-31 13:19:41 -0500

    fix

--- ies2ldt.py
+++ ies2ldt.py
@@ -1,8 +1,8 @@
 # https://docs.agi32.com/PhotometricToolbox/Content/Open_Tool/eulumdat_file_format.htm
 # https://docs.agi32.com/AGi32/Content/references/IDH_iesna_standard_file_format.htm
 
-f_ies = open("original.ies", "r")
-f_ldt = open("result.ldt", "w")
+f_org = open("original.ies", "r")
+f_res = open("result.ldt", "w")
 
 def num2str(n):									# convert number to string
 	s = str(int(n * 10000) / 10000)
@@ -19,9 +19,9 @@ lum_num = ""
 serial_number = ""
 type_lamp = ""
 
-f_ies.readline()
+f_org.readline()
 while True:
-	line = f_ies.readline()
+	line = f_org.readline()
 	if line[0] == '[':
 		cls_index = line.index(']')
 		keyword = line[1 : cls_index]
@@ -43,7 +43,7 @@ while True:
 	if line[0:4] == "TILT":
 		break
 
-line = f_ies.readline()
+line = f_org.readline()
 items = line.split()
 cnt_lamps = int(items[0])				# number of lamps
 lumens = int(items[1])					# lumens/lamp
@@ -56,23 +56,23 @@ width = float(items[7])					# width
 length = float(items[8])				# length
 height = float(items[9])				# height
 
-line = f_ies.readline()
+line = f_org.readline()
 items = line.split()
 bal_fac = float(items[0])				# ballast factor
 fut_use = float(items[1])				# future use
 input_watts = float(items[2])		# input watts
 
-line = f_ies.readline()
+line = f_org.readline()
 ver_angles = [float(i) for i in line.split()]	# vertical angles
 while True:
-	line = f_ies.readline()
+	line = f_org.readline()
 	if line[0] != ' ':
 		break
 	ver_angles += [float(i) for i in line.split()]
 
 hor_angles = [float(i) for i in line.split()]	# horizontal angles
 while True:
-	line = f_ies.readline()
+	line = f_org.readline()
 	if line[0] != ' ':
 		break
 	hor_angles += [float(i) for i in line.split()]
@@ -82,45 +82,45 @@ angles = []
 for i in range(cnt_hor_angles):								# candela values
 	angles.append([float(j) for j in line.split()])
 	while True:
-		line = f_ies.readline()
+		line = f_org.readline()
 		if line == "" or line[0] != ' ':
 			break
 		angles[i] += [float(j) for j in line.split()]
 
-f_ldt.write(manufac + "\n")												# Company
-f_ldt.write("2\n0\n")															# Ityp, Isym
-f_ldt.write(num2str(cnt_hor_angles - 1) + "\n")		# Mc - Remove 360 deg
-f_ldt.write(num2str(hor_angles[1] - hor_angles[0]) + "\n")	# Dc
-f_ldt.write(num2str(cnt_ver_angles) + "\n")				# Ng
-f_ldt.write(num2str(ver_angles[1] - ver_angles[0]) + "\n")	# Dg
-f_ldt.write(serial_number + "\n")									# Measurement report number
-f_ldt.write(luminaire + "\n")											# Luminaire name
-f_ldt.write(lum_num + "\n")												# Luminaire number
-f_ldt.write("result.ldt\n")												# File name
-f_ldt.write(issue_date + " - " + test + "\n")			# Date/user
-f_ldt.write(num2str(length * 1000) + "\n")				# Length/diameter of luminaire (mm)
-f_ldt.write(num2str(width * 1000) + "\n")					# b - Width of luminaire (mm) (b = 0 for circular luminaire)
-f_ldt.write(num2str(height * 1000) + "\n")				# Height of luminaire (mm)
-f_ldt.write(num2str(length * 1000) + "\n")				# Length/diameter of luminous area (mm)
-f_ldt.write(num2str(width * 1000) + "\n")					# b1 - Width of luminous area (mm) (b1 = 0 for circular luminous area of luminaire)
-f_ldt.write("0\n0\n0\n0\n")												# Height of luminous area C-planes (mm)
-f_ldt.write("100\n100\n")													# DFF, LORL
-f_ldt.write(num2str(multiplier) + "\n")						# Conversion factor for luminous intensities (depending on measurement)
-f_ldt.write("0\n1\n")															# Tilt of luminaire during measurement, Number of standard sets of lamps
-f_ldt.write(num2str(cnt_lamps) + "\n")						# Number of lamps
-f_ldt.write(type_lamp + "\n")											# Type of lamps
-f_ldt.write(num2str(lumens) + "\n")								# Total luminous flux of lamps (lm)
-f_ldt.write("\n\n")																# Color appearance, Color rendering group
-f_ldt.write(num2str(input_watts) + "\n")					# Wattage including ballast (W)
+f_res.write(manufac + "\n")												# Company
+f_res.write("2\n0\n")															# Ityp, Isym
+f_res.write(num2str(cnt_hor_angles - 1) + "\n")		# Mc - Remove 360 deg
+f_res.write(num2str(hor_angles[1] - hor_angles[0]) + "\n")	# Dc
+f_res.write(num2str(cnt_ver_angles) + "\n")				# Ng
+f_res.write(num2str(ver_angles[1] - ver_angles[0]) + "\n")	# Dg
+f_res.write(serial_number + "\n")									# Measurement report number
+f_res.write(luminaire + "\n")											# Luminaire name
+f_res.write(lum_num + "\n")												# Luminaire number
+f_res.write("result.ldt\n")												# File name
+f_res.write(issue_date + " - " + test + "\n")			# Date/user
+f_res.write(num2str(length * 1000) + "\n")				# Length/diameter of luminaire (mm)
+f_res.write(num2str(width * 1000) + "\n")					# b - Width of luminaire (mm) (b = 0 for circular luminaire)
+f_res.write(num2str(height * 1000) + "\n")				# Height of luminaire (mm)
+f_res.write(num2str(length * 1000) + "\n")				# Length/diameter of luminous area (mm)
+f_res.write(num2str(width * 1000) + "\n")					# b1 - Width of luminous area (mm) (b1 = 0 for circular luminous area of luminaire)
+f_res.write("0\n0\n0\n0\n")												# Height of luminous area C-planes (mm)
+f_res.write("100\n100\n")													# DFF, LORL
+f_res.write(num2str(multiplier) + "\n")						# Conversion factor for luminous intensities (depending on measurement)
+f_res.write("0\n1\n")															# Tilt of luminaire during measurement, Number of standard sets of lamps
+f_res.write(num2str(cnt_lamps) + "\n")						# Number of lamps
+f_res.write(type_lamp + "\n")											# Type of lamps
+f_res.write(num2str(lumens) + "\n")								# Total luminous flux of lamps (lm)
+f_res.write("\n\n")																# Color appearance, Color rendering group
+f_res.write(num2str(input_watts) + "\n")					# Wattage including ballast (W)
 for i in range(10):																# DR - Direct ratios for room indices
-	f_ldt.write("1\n")
+	f_res.write("1\n")
 for i in range(len(hor_angles)):									# Angles C (beginning with 0 degrees)
-	f_ldt.write(num2str(hor_angles[i]) + "\n")
+	f_res.write(num2str(hor_angles[i]) + "\n")
 for i in range(len(ver_angles)):									# Angles G (beginning with 0 degrees)
-	f_ldt.write(num2str(ver_angles[i]) + "\n")
+	f_res.write(num2str(ver_angles[i]) + "\n")
 for i in range(len(angles)):											# Luminous intensity distribution (cd/1000 lumens)
 	for j in range(len(angles[i])):
-		f_ldt.write(num2str(angles[i][j] / lumens * 1000) + "\n")
+		f_res.write(num2str(angles[i][j] / lumens * 1000) + "\n")
 
-f_ies.close()
-f_ldt.close()
+f_org.close()
+f_res.close()
--- ldt2ies.py
+++ ldt2ies.py
@@ -1,8 +1,8 @@
 # https://docs.agi32.com/PhotometricToolbox/Content/Open_Tool/eulumdat_file_format.htm
 # https://docs.agi32.com/AGi32/Content/references/IDH_iesna_standard_file_format.htm
 
-f_ldt = open("original.ldt", "r")
-f_ies = open("result.ies", "w")
+f_org = open("original.ldt", "r")
+f_res = open("result.ies", "w")
 
 def num2str(n):									# convert number to string
 	s = str(int(n * 10000) / 10000)
@@ -11,59 +11,59 @@ def num2str(n):									# convert number to string
 	else:
 		return s
 
-company_name = f_ldt.readline()
-ltyp = int(f_ldt.readline())		# Type indicator
-lsym = int(f_ldt.readline())		# Symmetry indicator
-mc = int(f_ldt.readline())			# Number of C-planes between 0 and 360 degrees (usually 24 for interior, 36 for road lighting luminaires)
-dc = float(f_ldt.readline())		# Distance between C-planes
-ng = int(f_ldt.readline())			# Number of luminous intensities in each C-plane
-dg = float(f_ldt.readline())		# Distance between luminous intensities per C-plane
-meas_report = f_ldt.readline()	# Measurement report number
-lum_name = f_ldt.readline()			# Luminaire name
-lum_num = f_ldt.readline()			# Luminaire number
-file_name = f_ldt.readline()		# File name
-date = f_ldt.readline()					# Date/user
+company_name = f_org.readline()
+ltyp = int(f_org.readline())		# Type indicator
+lsym = int(f_org.readline())		# Symmetry indicator
+mc = int(f_org.readline())			# Number of C-planes between 0 and 360 degrees (usually 24 for interior, 36 for road lighting luminaires)
+dc = float(f_org.readline())		# Distance between C-planes
+ng = int(f_org.readline())			# Number of luminous intensities in each C-plane
+dg = float(f_org.readline())		# Distance between luminous intensities per C-plane
+meas_report = f_org.readline()	# Measurement report number
+lum_name = f_org.readline()			# Luminaire name
+lum_num = f_org.readline()			# Luminaire number
+file_name = f_org.readline()		# File name
+date = f_org.readline()					# Date/user
 temp = date.find("-")
 if temp >= 0:
 	user = date[temp + 2 : len(date) - 1]	# User
 	date = date[0 : temp - 1] + "\n"			# Date
 else:
 	user = ""
-length_lum = float(f_ldt.readline())					# Length/diameter of luminaire (mm)
-width_lum = float(f_ldt.readline())						# b - Width of luminaire (mm) (b = 0 for circular luminaire)
-height_lum = float(f_ldt.readline())					# Height of luminaire (mm)
-length_lumarea = float(f_ldt.readline())			# Length of luminaire (mm)
-width_lumarea = float(f_ldt.readline())				# Width of luminous area (mm) (b1 = 0 for circular luminous area of luminaire)
-height_lumarea_c0 = float(f_ldt.readline())		# Height of luminous area C0-plane (mm)
-height_lumarea_c90 = float(f_ldt.readline())	# Height of luminous area C90-plane (mm)
-height_lumarea_c180 = float(f_ldt.readline())	# Height of luminous area C180-plane (mm)
-height_lumarea_c270 = float(f_ldt.readline())	# Height of luminous area C270-plane (mm)
-dff = float(f_ldt.readline())				# DFF - Downward flux fraction (%)
-lorl = float(f_ldt.readline())			# LORL - Light output ratio luminaire (%)
-cffli = float(f_ldt.readline())			# Conversion factor for luminous intensities (depending on measurement)
-toldm = float(f_ldt.readline())			# Tilt of luminaire during measurement (road lighting luminaires)
-num_set = int(f_ldt.readline())			# n - Number of standard sets of lamps
-num_lam = int(f_ldt.readline())			# Number of lamps
-type_lam = f_ldt.readline()					# Type of lamps
-tlfl = float(f_ldt.readline())			# Total luminous flux of lamps (lm)
-col_temp = float(f_ldt.readline())	# Color appearance / color temperature of lamps
-col_ren = float(f_ldt.readline())		# Color rendering group / color rendering index
-power = float(f_ldt.readline())			# Wattage including ballast (W)
+length_lum = float(f_org.readline())					# Length/diameter of luminaire (mm)
+width_lum = float(f_org.readline())						# b - Width of luminaire (mm) (b = 0 for circular luminaire)
+height_lum = float(f_org.readline())					# Height of luminaire (mm)
+length_lumarea = float(f_org.readline())			# Length of luminaire (mm)
+width_lumarea = float(f_org.readline())				# Width of luminous area (mm) (b1 = 0 for circular luminous area of luminaire)
+height_lumarea_c0 = float(f_org.readline())		# Height of luminous area C0-plane (mm)
+height_lumarea_c90 = float(f_org.readline())	# Height of luminous area C90-plane (mm)
+height_lumarea_c180 = float(f_org.readline())	# Height of luminous area C180-plane (mm)
+height_lumarea_c270 = float(f_org.readline())	# Height of luminous area C270-plane (mm)
+dff = float(f_org.readline())				# DFF - Downward flux fraction (%)
+lorl = float(f_org.readline())			# LORL - Light output ratio luminaire (%)
+cffli = float(f_org.readline())			# Conversion factor for luminous intensities (depending on measurement)
+toldm = float(f_org.readline())			# Tilt of luminaire during measurement (road lighting luminaires)
+num_set = int(f_org.readline())			# n - Number of standard sets of lamps
+num_lam = int(f_org.readline())			# Number of lamps
+type_lam = f_org.readline()					# Type of lamps
+tlfl = float(f_org.readline())			# Total luminous flux of lamps (lm)
+col_temp = float(f_org.readline())	# Color appearance / color temperature of lamps
+col_ren = float(f_org.readline())		# Color rendering group / color rendering index
+power = float(f_org.readline())			# Wattage including ballast (W)
 dr = []
 for i in range(10):							# DR - Direct ratios for room indices k = 0.6 ... 5
-	dr.append(float(f_ldt.readline()))
+	dr.append(float(f_org.readline()))
 angle_c = []
 for i in range(mc):							# Angles C (beginning with 0 degrees)
-	angle_c.append(float(f_ldt.readline()))
+	angle_c.append(float(f_org.readline()))
 angle_c.append(360)							# Append 360 DEG
 angle_g = []
 for i in range(ng):							# Angles G (beginning with 0 degrees)
-	angle_g.append(float(f_ldt.readline()))
+	angle_g.append(float(f_org.readline()))
 lum_intensity = []
 for i in range(mc):							# Read luminous intensities
 	lum_intensity.append([])
 	for j in range(ng):
-		line = f_ldt.readline()
+		line = f_org.readline()
 		if line == "":
 			break
 		lum_intensity[i].append(float(line))
@@ -72,68 +72,68 @@ for i in range(mc):							# Read luminous intensities
 		break
 lum_intensity.append(lum_intensity[0])	# Append luminous intensities for 360 DEG
 
-f_ies.write("IESNA:LM-63-2002\n")
-f_ies.write("[TEST] ")
-f_ies.write(user + "\n")
-f_ies.write("[TESTLAB] ")
-f_ies.write(user + " - Test lab\n")
-f_ies.write("[MANUFAC] ")
-f_ies.write(company_name)
-f_ies.write("[ISSUEDATE] ")
-f_ies.write(date)
+f_res.write("IESNA:LM-63-2002\n")
+f_res.write("[TEST] ")
+f_res.write(user + "\n")
+f_res.write("[TESTLAB] ")
+f_res.write(user + " - Test lab\n")
+f_res.write("[MANUFAC] ")
+f_res.write(company_name)
+f_res.write("[ISSUEDATE] ")
+f_res.write(date)
 if len(lum_num) > 1:
-	f_ies.write("[LUMCAT] ")
-	f_ies.write(lum_num)
+	f_res.write("[LUMCAT] ")
+	f_res.write(lum_num)
 if len(lum_name) > 1:
-	f_ies.write("[LUMINAIRE] ")
-	f_ies.write(lum_name)
+	f_res.write("[LUMINAIRE] ")
+	f_res.write(lum_name)
 if len(type_lam) > 1:
-	f_ies.write("[LAMP] ")
-	f_ies.write(type_lam)
-f_ies.write("[_SERIALNUMBER] ")
-f_ies.write(meas_report)
-f_ies.write("TILT=NONE\n")
+	f_res.write("[LAMP] ")
+	f_res.write(type_lam)
+f_res.write("[_SERIALNUMBER] ")
+f_res.write(meas_report)
+f_res.write("TILT=NONE\n")
 
 if num_lam < 0:
-	f_ies.write("1 -1 ")
+	f_res.write("1 -1 ")
 else:
-	f_ies.write(num2str(num_lam) + " ")
-	f_ies.write(num2str(tlfl) + " ")	# 
-f_ies.write(num2str(cffli) + " ")		# multiplier
-f_ies.write(num2str(ng) + " ")
-f_ies.write(num2str(mc + 1) + " ")
-f_ies.write(num2str(1) + " ")		# photometric type
-f_ies.write(num2str(2) + " ")		# unit type
-f_ies.write(num2str(width_lumarea / 1000) + " ")
-f_ies.write(num2str(length_lumarea / 1000) + " ")
-f_ies.write(num2str(height_lum / 1000) + "\n")
+	f_res.write(num2str(num_lam) + " ")
+	f_res.write(num2str(tlfl) + " ")	# 
+f_res.write(num2str(cffli) + " ")		# multiplier
+f_res.write(num2str(ng) + " ")
+f_res.write(num2str(mc + 1) + " ")
+f_res.write(num2str(1) + " ")		# photometric type
+f_res.write(num2str(2) + " ")		# unit type
+f_res.write(num2str(width_lumarea / 1000) + " ")
+f_res.write(num2str(length_lumarea / 1000) + " ")
+f_res.write(num2str(height_lum / 1000) + "\n")
 
-f_ies.write("1.0 1.0 ")			# ballast factor, future use
-f_ies.write(num2str(power))	# input watts
-f_ies.write("\n")
+f_res.write("1.0 1.0 ")			# ballast factor, future use
+f_res.write(num2str(power))	# input watts
+f_res.write("\n")
 line = ""
 for angle in angle_g:				# vertical angles (maximum 240 characters in line)
 	if len(line + num2str(angle)) > 238:
-		f_ies.write(line + "\n")
+		f_res.write(line + "\n")
 		line = " "
 	line += num2str(angle) + " "
-f_ies.write(line + "\n")
+f_res.write(line + "\n")
 line = ""
 for angle in angle_c:				# horizontal angles (maximum 240 characters in line)
 	if len(line + num2str(angle)) > 238:
-		f_ies.write(line + "\n")
+		f_res.write(line + "\n")
 		line = " "
 	line += num2str(angle) + " "
-f_ies.write(line + "\n")
+f_res.write(line + "\n")
 for intens in lum_intensity:	# Candela values
 	line = ""
 	for inten in intens:
 		it = num2str(inten * float(tlfl) / 1000)
 		if len(line + it) > 238:
-			f_ies.write(line + "\n")
+			f_res.write(line + "\n")
 			line = " "
 		line += it + " "
-	f_ies.write(line + "\n")
+	f_res.write(line + "\n")
 
-f_ldt.close()
-f_ies.close()
+f_org.close()
+f_res.close()
